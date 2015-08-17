cb017f6d6e9c4f0e4b3a0f58175bdf2a36f24728
julien marechal <julien.marechal@pine.nl>
2015-08-17 09:59:28 +0200

    Cleanup + rebinds + line/cols

--- init.el
+++ init.el
@@ -1,14 +1,23 @@
+;; Builtins
+(custom-set-variables
+ '(linum-format "%d ")
+ '(column-number-mode 1)
+ '(show-paren-mode t)
+ '(auto-save-default nil)
+ )
+
+;; Package repos
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
 			 ("marmalade" . "https://marmalade-repo.org/packages/")
 			 ("melpa" . "http://melpa.org/packages/")))
-;; themes
+
+;; Themes
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+
+;; IDO
 (require 'ido)
 (ido-mode t)
 
-;; JEDI
-(add-hook 'python-mode-hook 'jedi:setup)
-(setq jedi:complete-on-dot t)       
 
 ;; Flycheck
 (add-hook 'after-init-hook #'global-flycheck-mode)
@@ -16,23 +25,20 @@
 ;; Monokai
 (load-theme 'monokai t)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("05c3bc4eb1219953a4f182e10de1f7466d28987f48d647c01f1f0037ff35ab9a" default)))
- '(show-paren-mode t)
- '(auto-save-default nil)
- )
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- ;; Theme
- )
+;; Rebinds
+(global-set-key (kbd "C-f") 'goto-line) ; Ctrl+f
 
 
+;;              PYTHON
+
+;; JEDI
+(add-hook 'python-mode-hook 'jedi:setup)
+(setq jedi:complete-on-dot t)
+
+;; Force indend 4 spaces
+(add-hook 'python-mode-hook
+	  (lambda ()
+	    (setq-default indent-tabs-mode nil)
+	    (setq-default tab-width 4)
+	    (setq-default python-indent 4)
+	    ))
