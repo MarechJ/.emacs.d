710c3c414426f9ee784dc0903afa111698ebac9e
Julien Marechal <julien.marechal@dmc.amcnetworks.com>
2015-10-14 14:07:30 +0200

    Added global linum-mode, removed menu and toolbar

--- init.el
+++ init.el
@@ -1,10 +1,20 @@
 ;; Builtins
 (custom-set-variables
- '(linum-format "%d ")
- '(column-number-mode 1)
- '(show-paren-mode t)
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(auto-save-default nil)
- )
+ '(column-number-mode 1)
+ '(inhibit-startup-screen t)
+ '(linum-format "%d ")
+ '(show-paren-mode t)
+ '(global-linum-mode 1)
+)
+
+;; Disable toolbars crap
+(menu-bar-mode -1)
+(tool-bar-mode -1)
 
 ;; Package repos
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
@@ -42,3 +52,9 @@
 	    (setq-default tab-width 4)
 	    (setq-default python-indent 4)
 	    ))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
